01f03a98b770ffdf181f0082c9ac8407b57f5b1b
Szücs József <szujoz95@gmail.com>
2018-12-06 10:03:36 +0100

    Line follow debug app is added.

---
+++ stm32/main-board/1_App/Src/app_linefollow.c
@@ -0,0 +1,112 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//!
+//!  \file      app_linefollow.c
+//!  \brief     
+//!  \details   
+//!
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Includes ------------------------------------------------------------------------------------------------------------
+
+#include "app_common.h"
+#include "app_linefollow.h"
+#include "line.h"
+#include "steer.h"
+#include "trace.h"
+#include "motor.h"
+#include "controller.h"
+
+// Defines -------------------------------------------------------------------------------------------------------------
+
+// Typedefs ------------------------------------------------------------------------------------------------------------
+
+// Local (static) & extern variables -----------------------------------------------------------------------------------
+
+//static double Tsrv;		// sec
+//static double Ksrv;		//
+
+static double p_a;		// m
+static double p_meas;	// m
+static double e;		// m
+static double p;		// m
+static double phi_a;	// deg
+static double v;		// m/s
+static double L;		// m
+
+static double Kp;		//
+//static double Kd;		//
+static double Td;		// sec
+static double T;		// sec
+
+static cFirstOrderTF contrPD;
+
+// Local (static) function prototypes ----------------------------------------------------------------------------------
+
+static void Task_LineFollow (void* p);
+
+// Global function definitions -----------------------------------------------------------------------------------------
+
+void TaskInit_LineFollow (void)
+{
+	//Tsrv = 0.007;
+	//Ksrv = 60;
+
+	p_a   = 0;
+	e 	  = 0;
+	p 	  = -0.1;
+	phi_a = 0;
+	v 	  = 2.5;
+	L 	  = 0.275;
+
+	Kp = -4/(v*L);
+//	Kd = 23;
+	Td = 0.0217;
+	T  = 10*Td;
+
+	contrPD.an_past = 0;
+	contrPD.bn_past = 0;
+	contrPD.a1 = T;
+	contrPD.b0 = Kp;
+	contrPD.b1 = Kp*Td;
+
+    xTaskCreate(Task_LineFollow,
+                "TASK_LINE_FOLLOW",
+                DEFAULT_STACK_SIZE,
+                NULL,
+                TASK_LINE_FOLLOW_PRIO,
+                NULL);
+}
+
+uint8_t rxBuf[3];
+
+static void Task_LineFollow (void* p)
+{
+    (void)p;
+
+    uint8_t pwm = 20;
+
+    while (1)
+    {
+    	// Get new measurements of the line p distance.
+    	//p_meas = lineGet().d;
+    	p_meas = -0.1;
+    	traceBluetooth(BCM_LOG_LINE_D, &p_meas);
+
+    	// Calculate control error.
+    	e = p_a - p_meas;
+
+    	// Give the error to the controller and receive new
+    	phi_a = controllerTransferFunction(&contrPD, e);
+
+    	steerSetAngle(3.14159265359/180 * phi_a);
+    	traceBluetooth(BCM_LOG_SERVO_ANGLE, &phi_a);
+
+    	motorSetDutyCycle(pwm);
+    	traceBluetooth(BCM_LOG_CTR_MTR_CURR, &pwm);
+
+    	//TODO const to define
+    	vTaskDelay(5);
+    }
+}
+
+// Local (static) function definitions ---------------------------------------------------------------------------------
