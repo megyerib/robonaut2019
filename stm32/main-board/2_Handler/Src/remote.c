a51b7bb30d1a4d9a099b20f8648db685e3cb843c
Balázs Megyeri <m1eri@users.noreply.github.com>
2019-01-22 16:35:42 +0100

    New remote controller calibrated (algorithm changed a bit too)

--- stm32/main-board/2_Handler/Src/remote.c
+++ stm32/main-board/2_Handler/Src/remote.c
@@ -12,21 +12,19 @@
 
 // Defines -------------------------------------------------------------------------------------------------------------
 
-  #define PERIOD 16320 /* us */
-  #define MIDDLE  1495 /* us */
-//#define MIN      995 /* us */
-//#define MAX     2080 /* us */
+  #define MIDDLE  2945 /* us */
+  #define MAX     4060 /* us */
 
-  #define MARGIN   200 /* us */
+  #define MARGIN   250 /* us */
 
-  #define HIGH_THR (MIDDLE + MARGIN)
-  #define LOW_THR  (PERIOD - MIDDLE - MARGIN)
+  #define ON_THR     (MIDDLE + MARGIN)
+  #define UPPER_THR  (MAX + MARGIN)
 
 // Typedefs ------------------------------------------------------------------------------------------------------------
 
 // Local (static) & extern variables -----------------------------------------------------------------------------------
 
-static int input_capture = 0;
+static int remote_state  = 0;
 
 // Local (static) function prototypes ----------------------------------------------------------------------------------
 
@@ -39,14 +37,7 @@ void remoteInit()
 
 int remoteGetState()
 {
-	if (input_capture > HIGH_THR && input_capture < LOW_THR)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+	return remote_state;
 }
 
 // Interrupt callback
@@ -54,8 +45,28 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
 	if (htim->Instance == TIM1)
 	{
-		input_capture = __HAL_TIM_GET_COMPARE(&htim1, TIM_CHANNEL_4);
+		int input_capture = __HAL_TIM_GET_COMPARE(&htim1, TIM_CHANNEL_4);
 		__HAL_TIM_SetCounter(&htim1, 0);
+
+
+		/* // Some code for calibration (from debugger)
+		static int j;
+
+		j++;
+
+		if (j == 5)
+		{
+			j = 0; // Set breakpoint here
+		}*/
+
+		if (input_capture < UPPER_THR && input_capture > ON_THR)
+		{
+			remote_state = 1;
+		}
+		else if (input_capture < ON_THR)
+		{
+			remote_state = 0;
+		}
 	}
 }
 
