533b2c4c24bc1318d7396a46164eee69bf597c81
Szücs József <szujoz95@gmail.com>
2019-01-30 14:57:21 +0100

    CDT APP has setting define to choose between usb and bt trace.

--- stm32/main-board/1_App/Src/app_cdt.c
+++ stm32/main-board/1_App/Src/app_cdt.c
@@ -8,18 +8,20 @@
 
 // Includes ------------------------------------------------------------------------------------------------------------
 
-#include <bsp_servo.h>
-#include <bsp_sharp.h>
 #include "app_cdt.h"
-
 #include "app_common.h"
 #include "trace.h"
 #include "bsp_uart.h"
 
 // Defines -------------------------------------------------------------------------------------------------------------
+
+#define	APP_TRACE_BT_ON			1
+#define APP_TRACE_USB_ON		0
+
 // Typedefs ------------------------------------------------------------------------------------------------------------
 // Local (static) & extern variables -----------------------------------------------------------------------------------
 
+//______________________________________________________________________ TX SIDE
 QueueHandle_t qNaviN_f;
 QueueHandle_t qNaviE_f;
 QueueHandle_t qNaviPSI_f;
@@ -69,39 +71,35 @@ QueueHandle_t qSRunGetKp_f;
 QueueHandle_t qSRunGetKd_f;
 QueueHandle_t qSRunGetSpeed_u32;
 
-uint8_t btRxBuffer[TRACE_REC_MSG_SIZE];
+//______________________________________________________________________ RX SIDE
+
 
 QueueHandle_t qRecData;
-cTRACE_RX_DATA recData;
-bool btReceived = false;
-double cntr = 0;
 
-// USB______________________________________
+uint8_t btRxBuffer[TRACE_REC_MSG_SIZE];
 
-bool usbSent = false;
-bool usbRec = false;
+cTRACE_RX_DATA btRxData;
+
+bool btMsgReceived = false;
+
+//______________________________________________________________________ USB
 
 extern UART_HandleTypeDef huart5;
+
 uint8_t usbRxBuffer[TRACE_REC_MSG_SIZE];
-uint8_t usbTxBuffer[BT_LOG_SIZE+5];
-cMEASUREMENT_DIST sharp;
-double servo = 60;
-double encVel = 0;
-double motor = 0;
 
-cTRACE_RX_DATA usbStruct;
+cTRACE_RX_DATA usbRxStruct;
 
-// END___________________________-
+bool usbRec = false;
 
 // Local (static) function prototypes ----------------------------------------------------------------------------------
 // Global function definitions -----------------------------------------------------------------------------------------
 
-
-//! @brief	Initializes the Task_CarDiagnosticsTool task.
 void TaskInit_CarDiagnosticsTool(void)
 {
 	traceInit();
 
+	//______________________________________________________________________ TX SIDE
 	qNaviN_f       			= xQueueCreate( 1, sizeof( float ) );		// 1
 	qNaviE_f       			= xQueueCreate( 1, sizeof( float ) );
 	qNaviPSI_f     		 	= xQueueCreate( 1, sizeof( float ) );
@@ -140,7 +138,7 @@ void TaskInit_CarDiagnosticsTool(void)
 	qMazeActSpeed_u32  		= xQueueCreate( 1, sizeof( uint32_t ) );
 	qMazeInclinSegment_u32  = xQueueCreate( 1, sizeof( uint32_t ) );	// 34
 
-	qSRunMainSM_u32   		= xQueueCreate( 1, sizeof( uint32_t ) );		// 35
+	qSRunMainSM_u32   		= xQueueCreate( 1, sizeof( uint32_t ) );	// 35
 	qSRunActState_u32   	= xQueueCreate( 1, sizeof( uint32_t ) );
 	qSRunActP_f		    	= xQueueCreate( 1, sizeof( float ) );
 	qSRunActKp_f		    = xQueueCreate( 1, sizeof( float ) );
@@ -149,27 +147,25 @@ void TaskInit_CarDiagnosticsTool(void)
 	qSRunGetP_f		    	= xQueueCreate( 1, sizeof( float ) );
 	qSRunGetKp_f		    = xQueueCreate( 1, sizeof( float ) );
 	qSRunGetKd_f		    = xQueueCreate( 1, sizeof( float ) );
-	qSRunGetSpeed_u32   	= xQueueCreate( 1, sizeof( uint32_t ) );		// 44
+	qSRunGetSpeed_u32   	= xQueueCreate( 1, sizeof( uint32_t ) );	// 44
 
 	// Atollic debug queues
-	/*vQueueAddToRegistry(qNaviN_f,		   "qNaviN_f");
-	vQueueAddToRegistry(qNaviE_f,  		   "qNaviE_f");
-	vQueueAddToRegistry(qNaviPhi_f,   	   "qNaviPhi_f");
-	vQueueAddToRegistry(qEncVel_f, 		   "EncVel");
-	vQueueAddToRegistry(qTof1Distance_u32, "Tof1Dist");
-	vQueueAddToRegistry(qTof2Distance_u32, "Tof2Dist");
-	vQueueAddToRegistry(qTof3Distance_u32, "Tof3Dist");
-	vQueueAddToRegistry(qMtrSrvCurr_u32,   "MtrSrvCurr");
-	vQueueAddToRegistry(qCtrlMtrCurr_f,    "CtrlMtrCurr");*/
+	vQueueAddToRegistry(qNaviN_f,		    "qNaviN_f");
+	vQueueAddToRegistry(qNaviE_f,  		    "qNaviE_f");
+	vQueueAddToRegistry(qNaviPSI_f,   	    "qNaviPsi_f");
+	vQueueAddToRegistry(qEncV_f, 		    "EncV");
+	vQueueAddToRegistry(qDistToF1_u32, 	    "DistToF1");
+	vQueueAddToRegistry(qDistSharp1_u32,    "DistSharp1");
+	vQueueAddToRegistry(qLineMainLinePos_f, "MainLinePos");
+	vQueueAddToRegistry(qMazeActState_u32,  "MazeActState");
+	vQueueAddToRegistry(qSRunGetP_f,    	"SRunGetP");
 
+	//______________________________________________________________________ RX SIDE
 	qRecData = xQueueCreate( 1, sizeof( cTRACE_RX_DATA ) );
 
 	vQueueAddToRegistry(qRecData, "RecData");
 
-	// TODO sharpTriggerAdc();
-
-	//TODO
-	sharp.Distance = 40;
+	//______________________________________________________________________ TASK CREATE
 
 	xTaskCreate(Task_CarDiagnosticsTool,
 				"TASK_CAR_DIAGNOSTICS_TOOL",
@@ -179,132 +175,67 @@ void TaskInit_CarDiagnosticsTool(void)
 				NULL);
 }
 
-
-//! @brief	Communicates with the CDT client application via bluetooth.
 void Task_CarDiagnosticsTool(void* p)
 {
 	(void)p;
 
-	bspUartReceive_IT(Uart_USB, usbRxBuffer, TRACE_REC_MSG_SIZE);
-	//bspUartReceive_IT(Uart_Bluetooth, btRxBuffer, TRACE_REC_MSG_SIZE);
-
-	uint8_t btSentMessage[BT_LOG_SIZE+7];
-
-	while (1)
+	//_______________________________________________________________________ TRACE SETTINGS
+	if (APP_TRACE_BT_ON == 1)
 	{
-		traceFlushData();
-		//bspUartTransmit_IT(Uart_USB, btSentMessage, BT_LOG_SIZE+7);
-
-		if (usbRec == true)
-		{
-			usbRec = false;
-
-			traceProcessRxData(usbRxBuffer);
-
-			recData = traceGetRxData();
-			//xQueuePeek(qRecData, &recData, 0);
-
-			bspUartReceive_IT(Uart_USB, usbRxBuffer, TRACE_REC_MSG_SIZE);
-		}
-
+		bspUartReceive_IT(Uart_Bluetooth, btRxBuffer, TRACE_REC_MSG_SIZE);
+	}
+	else
+	{
 		bspUartReceive_IT(Uart_USB, usbRxBuffer, TRACE_REC_MSG_SIZE);
-		vTaskDelay(100);
 	}
 
-/*	sharp = sharpGetMeasurement();
-
-	while(1)
+	//_______________________________________________________________________ TRACE TASK
+	while (1)
 	{
-		if (true)//bcmBluetoothConnected())
+		if (bspBluetoothConnected() == true && APP_TRACE_BT_ON == 1)
 		{
+			// Bluetooth trace to Car Diagnostics Tool.
+
+			traceFlushData();
+
+			if (btMsgReceived == true)
+			{
+				btMsgReceived = false;
+
+				traceProcessRxData(btRxBuffer);
+
+				btRxData = traceGetRxData();
+			}
+
+			bspUartReceive_IT(Uart_Bluetooth, btRxBuffer, TRACE_REC_MSG_SIZE);
+		}
+		else if (APP_TRACE_USB_ON == 1)
+		{
+			// USB trace.
+
+			traceFlushData();
+
 			if (usbRec == true)
 			{
 				usbRec = false;
 
-				usbStruct = traceProcessRxData(usbRxBuffer);
+				traceProcessRxData(usbRxBuffer);
+
+				usbRxStruct = traceGetRxData();
 			}
 
-			if (btReceived == true)
-			{
-				btReceived = false;
-
-				recData = traceProcessRxData(btRxBuffer);
-
-				xQueueOverwrite(qRecData, (void*) &recData);
-
-				btReceived = false;
-
-				// TODO debug?
-				bspUartReceive_IT(Uart_Bluetooth, btRxBuffer, TRACE_REC_MSG_SIZE);
-				bspUartTransmit_IT(Uart_USB, usbTxBuffer, TRACE_REC_MSG_SIZE);
-			}
-
-			// SEND bt and USB TODO
-			traceFlushData();
-
-			if(recData.RecCmdAccelerate && recData.RecDataAccelerate >= 0 && recData.RecDataAccelerate < 100)
-			{
-				motor = (double)recData.RecDataAccelerate;
-//				xQueueOverwrite(qAlkalmazasDemoMotor, &motor);
-				recData.RecCmdAccelerate = false;
-			}
-
-			if(recData.RecCmdSteer && recData.RecDataSteer >= 0 && recData.RecDataSteer < 180)
-			{
-				servo = (double)recData.RecDataSteer * 3.14/180 - 3.14/2;
-				servoSetAngle(servo);
-				recData.RecCmdSteer = false;
-			}
-
-			sharp = sharpGetMeasurement();
-
-			if(sharp.Distance < 40)
-			{
-				//uint8_t motor_stop = 0;
-				traceBluetooth(BCM_LOG_SHARP_COLLISION_WARNING, (void*) true);
-	//			xQueueOverwrite(qAlkalmazasDemoMotor, &motor_stop);
-			}
-			else
-			{
-				traceBluetooth(BCM_LOG_SHARP_COLLISION_WARNING, (void*) true);
-			}
-
-			traceBluetooth(BCM_LOG_SHARP_DISTANCE, &sharp);
-			traceBluetooth(BCM_LOG_ENC_VEL, &motor);
-			traceBluetooth(BCM_LOG_SERVO_ANGLE, &servo);
-
-			// TODO debug?
-			bspUartReceive_IT(Uart_Bluetooth, btRxBuffer, TRACE_REC_MSG_SIZE);
 			bspUartReceive_IT(Uart_USB, usbRxBuffer, TRACE_REC_MSG_SIZE);
 		}
-
-
-		if(recData.RecDataSteer > 0 && recData.RecDataSteer < 180)
+		else
 		{
-			servo = (double)usbStruct.RecDataSteer;
-			servoSetAngle(usbStruct.RecDataSteer);
+			// Trace is turned off.
 		}
 
-
-		cntr++;
-
-		sharpTriggerAdc();
-
-
-		traceBluetooth(BCM_LOG_SERVO_ANGLE, &servo);
-		traceBluetooth(BCM_LOG_ENC_VEL, &encVel);
-		traceBluetooth(BCM_LOG_SHARP_DISTANCE, &sharp);
-
-		vTaskDelay(200);
-	}*/
+		vTaskDelay(100);
+	}
 }
 
 // Local (static) function definitions ---------------------------------------------------------------------------------
-
-void bspUsbTxCpltCallback()
-{
-	usbSent = true;
-}
 
 void bspUsbRxCpltCallback ()
 {
@@ -313,21 +244,5 @@ void bspUsbRxCpltCallback ()
 
 void bspBluetoothRxCpltCallback ()
 {
-	btReceived = true;
-
-	//memcpy(usbRxBuffer, btRxBuffer, TRACE_REC_MSG_SIZE);
+	btMsgReceived = true;
 }
-
-// THIS ONE WORKS!!!
-/*uint8_t uzenet[19] = "AT+AB GetBDAddress\r";
-uint8_t valasz[30];
-
-HAL_UART_Transmit_IT(&huart5, uzenet, sizeof(uzenet));
-HAL_UART_Receive(&huart5, valasz, sizeof(valasz), 5000);
-
-
-uint8_t txBtMsgRename[25] = "AT+AB LocalName Override\r";
-uint8_t rxBtRename[30];
-
-HAL_UART_Receive_IT(&huart5, rxBtRename, sizeof(rxBtRename));
-HAL_UART_Transmit_IT(&huart5, txBtMsgRename, sizeof(txBtMsgRename));*/
